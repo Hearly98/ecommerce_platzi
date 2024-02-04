7f9a2fa2570d071c6f5f85b61fe03156ab0c1d4f
Sebastian <shuertas981@gmail.com>
2024-02-03 22:41:44 -0500

    agregando un navbar

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-product',
@@ -9,10 +10,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-  @Input({required:true}) img:string='';//decorador input que viene desde el angular core
-  @Input({required:true}) price:number=0;
-  @Input({required:true}) title:string=''
-
+  @Input({required:true}) product!:Product;//decorador input que viene desde el angular core
 
   //decorador output que viene desde el angular core
   @Output() addToCart = new EventEmitter();// permitira enviarle cosas del hijo al padre
@@ -20,6 +18,6 @@ export class ProductComponent {
   //funcion
   addToCartHandler(){
     console.log('click form child');
-    this.addToCart.emit('Hola este es una mensaje desde el hijo'+ this.title) 
+    this.addToCart.emit('Hola este es una mensaje desde el hijo'+ this.product.title) 
   }
 }
--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, Input, SimpleChanges, signal } from '@angular/core';
+import { Console } from 'console';
 
 @Component({
   selector: 'app-counter',
@@ -13,6 +14,8 @@ export class CounterComponent {
 
 @Input({required:true}) duration =0;
 @Input({required:true}) message='';
+counter = signal(0);
+counterRef:number | undefined;
 
 
 constructor(){
@@ -28,5 +31,41 @@ ngOnChanges(changes: SimpleChanges){//SimplesChanges sirve te avisa que ha ocurr
 console.log('ngOnChanges');
 console.log('-'.repeat(10));
 console.log(changes);//para saber que cosas estan cambiando
+const duration = changes['duration'];
+console.log(duration);
+if(duration && duration.currentValue !== duration.previousValue){
+  this.doSomething();
+}
+}
+ngOnInit(){
+  //despues del render
+  //corre 1 vez
+  //async, then, subs
+  console.log('ngOnInit');
+  console.log('-'.repeat(10));
+  console.log('duration =>', this.duration);
+  console.log('message =>', this.message);
+  this.counterRef=window.setInterval(()=>{
+    //correr cada cierto tiempo
+    console.log('run interval');
+    this.counter.update(statePrev=>statePrev+1);
+  },1000)
+}
+ngAfterViewInit(){
+  //after render
+  //preguntar si los hijos de este componentes ya fueron renderizados
+
+  console.log('ngAfterViewInit');
+  console.log('-'.repeat(10));
+}
+ngOnDestroy(){
+  //ver cuando el componente se destruye
+  console.log('ngOnDestroy');
+  console.log('-'.repeat(10));
+  window.clearInterval(this.counterRef);//para destruir el intervalo
+}
+
+doSomething(){
+  console.log('changeDuration');
 }
 }
